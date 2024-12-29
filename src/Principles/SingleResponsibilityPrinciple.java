a4d501f1b212cb2be4e017686b46bce74a2c378e
Maidlet <April.main@outlook.com>
2024-12-29 19:01:39 +0800

    + 单一职责原则（Single Responsibility Principle，SRP）

---
+++ src/Principles/SingleResponsibilityPrinciple.java
@@ -0,0 +1,16 @@
+package Principles;
+
+public class SingleResponsibilityPrinciple {
+    /*
+     * Single Responsibility Principle
+     * 单一职责原则（Single Responsibility Principle，SRP）单一功能原则：
+     * 规定一个类应该有且仅有一个引起它变化的原因，否则类应该被拆分
+     * There should never be more than one reason for a class to change
+     *
+     * 如果现在有个类，他负责了很多事情，可以打印日志，可以链接数据库，还提供了一些函数供其他类调用
+     * 如果现在我们要修改这个类，那么可能会出现问题，虽然功能各自独立改了就改了，无所谓，但项目打起来，就会非常的类
+     * 单一职责原则思想就是 低内聚，高耦合
+     */
+
+
+}
